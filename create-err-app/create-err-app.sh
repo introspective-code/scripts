dc0bdd28737516b585e0502ed8ba53d88b7a1354
Shahriyar Nasir <contact@snasir.ca>
2018-01-29 20:03:11 -0500

    Refactor the line lengths

--- create-err-app/create-err-app.sh
+++ create-err-app/create-err-app.sh
@@ -2,13 +2,21 @@
 
 APP_NAME=$1
 
+# Set up the backend
+
 express ${APP_NAME}
 cd ${APP_NAME}
 npm install
-npm install --save-dev @types/jest @types/supertest babel-eslint babel-preset-env eslint eslint-config-react-app eslint-config-standard eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-node eslint-plugin-promise eslint-plugin-react eslint-plugin-standard jest nodemon stylelint stylelint-config-standard supertest 
+npm install --save-dev @types/jest @types/supertest babel-eslint babel-preset-env eslint eslint-config-react-app \
+  eslint-config-standard eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-node \
+  eslint-plugin-promise eslint-plugin-react eslint-plugin-standard jest nodemon stylelint stylelint-config-standard \
+  supertest
+
+# Set up the frontend
 
 create-react-app client
 cd client
 npm install
-npm install --save classnames history material-ui material-ui-icons prop-types react-keypress react-redux react-router-redux redux redux-saga humps
-npm install --save-dev enzyme enzyme-adapter-react-16 redux-mock-store 
+npm install --save classnames history material-ui material-ui-icons prop-types react-keypress react-redux \
+  react-router-redux redux redux-saga humps
+npm install --save-dev enzyme enzyme-adapter-react-16 redux-mock-store
