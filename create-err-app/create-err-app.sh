e4f698c6bf8aeea3693394e1be7d8eff7adb9e81
Nayema Nasir <nayema.nasir89@gmail.com>
2018-01-30 17:00:19 -0500

    Specify material-ui and react-router-redux versions

--- create-err-app/create-err-app.sh
+++ create-err-app/create-err-app.sh
@@ -8,9 +8,9 @@ npx express-generator ${APP_NAME}
 cd ${APP_NAME}
 npm install
 npm install --save-dev @types/jest @types/supertest babel-eslint babel-preset-env eslint eslint-config-react-app \
-  eslint-config-standard eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-node \
-  eslint-plugin-promise eslint-plugin-react eslint-plugin-standard jest nodemon stylelint stylelint-config-standard \
-  supertest
+    eslint-config-standard eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-node \
+    eslint-plugin-promise eslint-plugin-react eslint-plugin-standard jest nodemon stylelint stylelint-config-standard \
+    supertest
 
 npx json -I -f package.json -e 'this.babel={ "presets": [ "env" ] }'
 npx json -I -f package.json -e 'this.eslintConfig={ "extends": [ "standard", "react-app" ], "env": { "jest": true } }'
@@ -22,6 +22,6 @@ npx json -I -f package.json -e 'this.jest={ "modulePathIgnorePatterns": [ "<root
 npx create-react-app client
 cd client
 npm install
-npm install --save classnames history material-ui material-ui-icons prop-types react-keypress react-redux \
-  react-router-redux redux redux-saga humps
+npm install --save classnames history material-ui@^1.0.0-beta.30 material-ui-icons prop-types react-keypress \
+    react-redux react-router-redux@next redux redux-saga humps
 npm install --save-dev enzyme enzyme-adapter-react-16 redux-mock-store
